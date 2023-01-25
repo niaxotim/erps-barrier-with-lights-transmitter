25d288e5a999fa9e09fc12029bafa63adfc511ba
Tim <58301440+niaxotim@users.noreply.github.com>
2023-01-25 11:25:17 +0000

    Blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("up")
+    Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Up)
 })
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("down")
+    Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
+})
+Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
+radio.setGroup(255)
